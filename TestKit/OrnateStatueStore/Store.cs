696c4c5296585a2fb1d4e3db2231f6cee21c93b7
Vincent Farah <vincent.farah@gbgplc.com>
2025-06-11 14:13:28 +0100

    Extract logical groupings

--- TestKit/OrnateStatueStore/Store.cs
+++ TestKit/OrnateStatueStore/Store.cs
@@ -24,43 +24,78 @@ public class Store
         UpdateQualityAfterSellInExpires(item);
     }
 
+    private static void UpdateQualityBeforeSellInChanges(Item item)
+    {
+        switch (item.Name)
+        {
+            case "Aged Brie":
+                UpdateAgedBrie(item);
+                break;
+            case "Backstage passes to concert":
+                UpdateBackstagePassesQuality(item);
+                break;
+            case "Fresh apples":
+                UpdateFreshApples(item);
+                break;
+            default:
+                UpdateDefault(item);
+                break;
+        }
+    }
+
     private static void UpdateQualityAfterSellInExpires(Item item)
     {
         if (item.SellIn >= 0) return;
         switch (item.Name)
         {
             case "Aged Brie":
-                item.IncreaseQuality();
+                UpdatedAgedBrieWhenExpired(item);
                 return;
             case "Backstage passes to concert":
-                item.Quality = 0;
+                UpdateExpiredConcert(item);
                 break;
             case "Fresh apples":
-                item.ReduceQuality(2);
+                UpdateExpiredFreshApples(item);
                 break;
             default:
-                item.ReduceQuality();
+                UpdateDefaultExpiredItem(item);
                 return;
         }
     }
 
-    private static void UpdateQualityBeforeSellInChanges(Item item)
+    private static void UpdateDefaultExpiredItem(Item item)
     {
-        switch (item.Name)
-        {
-            case "Aged Brie":
-                item.IncreaseQuality();
-                break;
-            case "Backstage passes to concert":
-                UpdateBackstagePassesQuality(item);
-                break;
-            case "Fresh apples":
-                item.ReduceQuality(2);
-                break;
-            default:
-                item.ReduceQuality();
-                break;
-        }
+        item.ReduceQuality();
+    }
+
+    private static void UpdateExpiredFreshApples(Item item)
+    {
+        item.ReduceQuality(2);
+    }
+
+    private static void UpdateExpiredConcert(Item item)
+    {
+        item.Quality = 0;
+    }
+
+    private static void UpdatedAgedBrieWhenExpired(Item item)
+    {
+        item.IncreaseQuality();
+    }
+
+    private static void UpdateDefault(Item item)
+    {
+        item.ReduceQuality();
+    }
+
+    private static void UpdateFreshApples(Item item)
+    {
+        item.ReduceQuality(2);
+    }
+
+    private static void UpdateAgedBrie(Item item)
+    {
+        item.IncreaseQuality();
     }
 
     private static void UpdateBackstagePassesQuality(Item item)
