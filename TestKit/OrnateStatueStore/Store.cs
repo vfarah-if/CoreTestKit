9308e53c8f23027e5996ff89636fcfd5185462ed
Vincent Farah <vincent.farah@gbgplc.com>
2025-06-11 15:41:35 +0100

    Clean up store

--- TestKit/OrnateStatueStore/Store.cs
+++ TestKit/OrnateStatueStore/Store.cs
@@ -3,6 +3,13 @@ namespace OrnateStatueStore;
 public partial class Store
 {
     private readonly IList<Item> _items;
+    private readonly Dictionary<string, IItemUpdater> _itemUpdaters = new()
+    {
+        ["Aged Brie"] = new AgedBrieUpdater(),
+        ["Backstage passes to concert"] = new BackstagePassesUpdater(),
+        ["Fresh apples"] = new FreshApplesUpdater(),
+        ["Diamond ring"] = new DiamondRingUpdater(),
+    };
 
     public Store(IList<Item> items)
     {
@@ -10,65 +17,10 @@ public partial class Store
     }
     public void UpdateQuality()
     {
-        var itemUpdaters = new Dictionary<string, IItemUpdater>()
-        {
-            ["Aged Brie"] = new AgedBrieUpdater(),
-            ["Backstage passes to concert"] = new BackstagePassesUpdater(),
-            ["Fresh apples"] = new FreshApplesUpdater(),
-            ["Diamond ring"] = new DiamondRingUpdater(),
-        };
         foreach (var item in _items)
         {
-            var updater = itemUpdaters.TryGetValue(item.Name, out var itemUpdater) ? itemUpdater : new DefaultItemUpdater();
+            var updater = _itemUpdaters.TryGetValue(item.Name, out var itemUpdater) ? itemUpdater : new DefaultItemUpdater();
             updater.Update(item);
-            // UpdateItem(item);
         }
     }
-
-    // private static void UpdateItem(Item item)
-    // {
-    //     if (item.Name == "Diamond ring") return;
-    //     UpdateQualityBeforeSellInChanges(item);
-    //     item.ReduceSellInByADay();
-    //     UpdateQualityAfterSellInExpires(item);
-    // }
-
-    // private static void UpdateQualityBeforeSellInChanges(Item item)
-    // {
-    //     switch (item.Name)
-    //     {
-    //         case "Aged Brie":
-    //             UpdateAgedBrie(item);
-    //             break;
-    //         case "Backstage passes to concert":
-    //             UpdateBackstagePassesQuality(item);
-    //             break;
-    //         case "Fresh apples":
-    //             UpdateFreshApples(item);
-    //             break;
-    //         default:
-    //             UpdateDefault(item);
-    //             break;
-    //     }
-    // }
-
-    // private static void UpdateQualityAfterSellInExpires(Item item)
-    // {
-    //     if (item.SellIn >= 0) return;
-    //     switch (item.Name)
-    //     {
-    //         case "Aged Brie":
-    //             UpdatedAgedBrieWhenExpired(item);
-    //             return;
-    //         case "Backstage passes to concert":
-    //             UpdateExpiredConcert(item);
-    //             break;
-    //         case "Fresh apples":
-    //             UpdateExpiredFreshApples(item);
-    //             break;
-    //         default:
-    //             UpdateDefaultExpiredItem(item);
-    //             return;
-    //     }
-    // }
 }
