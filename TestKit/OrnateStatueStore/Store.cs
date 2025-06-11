c5927bf8f9150402e87e2341bc06d9c76c675677
Vincent Farah <vincent.farah@gbgplc.com>
2025-06-11 09:04:04 +0100

    Refactor obvious ide issues

--- TestKit/OrnateStatueStore/Store.cs
+++ TestKit/OrnateStatueStore/Store.cs
@@ -3,80 +3,81 @@ namespace OrnateStatueStore;
 public class Store
 {
     private readonly IList<Item> _items;
+
     public Store(IList<Item> items)
     {
         _items = items;
     }
     public void UpdateQuality()
     {
-        for (var i = 0; i < _items.Count; i++)
+        foreach (var item in _items)
         {
-            if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to concert")
+            if (item.Name != "Aged Brie" && item.Name != "Backstage passes to concert")
             {
-                if (_items[i].Quality > 0)
+                if (item.Quality > 0)
                 {
-                    if (_items[i].Name != "Diamond ring")
+                    if (item.Name != "Diamond ring")
                     {
-                        _items[i].Quality = _items[i].Quality - 1;
+                        item.Quality -= 1;
                     }
                 }
             }
             else
             {
-                if (_items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    _items[i].Quality = _items[i].Quality + 1;
+                    item.Quality += 1;
 
-                    if (_items[i].Name == "Backstage passes to concert")
+                    if (item.Name == "Backstage passes to concert")
                     {
-                        if (_items[i].SellIn < 11)
+                        if (item.SellIn < 11)
                         {
-                            if (_items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                _items[i].Quality = _items[i].Quality + 1;
+                                item.Quality += 1;
                             }
                         }
 
-                        if (_items[i].SellIn < 6)
+                        if (item.SellIn < 6)
                         {
-                            if (_items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                _items[i].Quality = _items[i].Quality + 1;
+                                item.Quality += 1;
                             }
                         }
                     }
                 }
             }
 
-            if (_items[i].Name != "Diamond ring")
+            if (item.Name != "Diamond ring")
             {
-                _items[i].SellIn = _items[i].SellIn - 1;
+                item.SellIn -= 1;
             }
 
-            if (_items[i].SellIn < 0)
+            if (item.SellIn < 0)
             {
-                if (_items[i].Name != "Aged Brie")
+                if (item.Name != "Aged Brie")
                 {
-                    if (_items[i].Name != "Backstage passes to concert")
+                    if (item.Name != "Backstage passes to concert")
                     {
-                        if (_items[i].Quality > 0)
+                        if (item.Quality > 0)
                         {
-                            if (_items[i].Name != "Diamond ring")
+                            if (item.Name != "Diamond ring")
                             {
-                                _items[i].Quality = _items[i].Quality - 1;
+                                item.Quality -= 1;
                             }
                         }
                     }
                     else
                     {
-                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
+                        item.Quality -= item.Quality;
                     }
                 }
                 else
                 {
-                    if (_items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        _items[i].Quality = _items[i].Quality + 1;
+                        item.Quality += 1;
                     }
                 }
             }
