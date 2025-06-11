11f1f19a1d98afb1f1c856ef9cf29b513cd3f89f
Vincent Farah <vincent.farah@gbgplc.com>
2025-06-11 08:22:01 +0100

    Add a starter kit for doing the modified guilded rose

---
+++ TestKit/OrnateStatueStore/Store.cs
@@ -0,0 +1,85 @@
+namespace OrnateStatueStore;
+
+public class Store
+{
+    private readonly IList<Item> _items;
+    public Store(IList<Item> items)
+    {
+        _items = items;
+    }
+    public void UpdateQuality()
+    {
+        for (var i = 0; i < _items.Count; i++)
+        {
+            if (_items[i].Name != "Aged Brie" && _items[i].Name != "Backstage passes to concert")
+            {
+                if (_items[i].Quality > 0)
+                {
+                    if (_items[i].Name != "Diamond ring")
+                    {
+                        _items[i].Quality = _items[i].Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (_items[i].Quality < 50)
+                {
+                    _items[i].Quality = _items[i].Quality + 1;
+
+                    if (_items[i].Name == "Backstage passes to concert")
+                    {
+                        if (_items[i].SellIn < 11)
+                        {
+                            if (_items[i].Quality < 50)
+                            {
+                                _items[i].Quality = _items[i].Quality + 1;
+                            }
+                        }
+
+                        if (_items[i].SellIn < 6)
+                        {
+                            if (_items[i].Quality < 50)
+                            {
+                                _items[i].Quality = _items[i].Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (_items[i].Name != "Diamond ring")
+            {
+                _items[i].SellIn = _items[i].SellIn - 1;
+            }
+
+            if (_items[i].SellIn < 0)
+            {
+                if (_items[i].Name != "Aged Brie")
+                {
+                    if (_items[i].Name != "Backstage passes to concert")
+                    {
+                        if (_items[i].Quality > 0)
+                        {
+                            if (_items[i].Name != "Diamond ring")
+                            {
+                                _items[i].Quality = _items[i].Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        _items[i].Quality = _items[i].Quality - _items[i].Quality;
+                    }
+                }
+                else
+                {
+                    if (_items[i].Quality < 50)
+                    {
+                        _items[i].Quality = _items[i].Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+}
